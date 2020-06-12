4b6b525eae34aebc6ef284ae0f3b1b72f807a5e6
phanvietdung2101 <59188700+phanvietdung2101@users.noreply.github.com>
2020-06-12 10:07:33 +0700

    add create book form

--- src/app/create-book/create-book.component.ts
+++ src/app/create-book/create-book.component.ts
@@ -1,4 +1,7 @@
+import { IBook } from './../model/book';
+import { BookService } from './../book.service';
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-create-book',
@@ -6,10 +9,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-book.component.css']
 })
 export class CreateBookComponent implements OnInit {
-
-  constructor() { }
+  bookForm : FormGroup
+  constructor(private bookService: BookService, private fb: FormBuilder) { }
 
   ngOnInit(): void {
+    this.bookForm = this.fb.group({
+      title: this.fb.control('',[Validators.required]),
+      author: this.fb.control('',[Validators.required]),
+      description: this.fb.control('',[Validators.required]),
+    })
+  }
+
+  onSubmit(){
+    const {book} = this.bookForm;
+    this.bookService.createBook(book).subscribe(
+      data => {
+        this.bookForm.reset;
+      },
+      err => {
+        alert(err)
+      }
+    )
   }
 
 }
